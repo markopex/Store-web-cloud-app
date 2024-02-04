49aa55ea64e85577ff342fea8cb5337df431e962
markopex <mali.coveculjak.rs@gmail.com>
2024-02-04 19:34:04 +0100

    - Product and category services added

---
+++ StoreApp/Common/Services/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Common.Dto;
+using Common.Models;
+using Microsoft.ServiceFabric.Services.Remoting;
+using System.ServiceModel;
+
+namespace Common.Services
+{
+    [ServiceContract]
+    public interface IProductService : IService
+    {
+        [OperationContract]
+        Task<Product> GetProduct(int id);
+        [OperationContract]
+        Task<Product> AddProduct(CreateUpdateProductDto productDto);
+        [OperationContract]
+        Task<List<Product>> GetProducts();
+    }
+}
